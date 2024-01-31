440eaf6f2f3eac529fbc57dc3e2310872ac59cc8
aakriti verma <aakritiv0709@gmail.com>
2024-01-31 15:49:29 -0500

    custom api and error handling

--- src/db/index.js
+++ src/db/index.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
       `\n MongoDB connected !! DB host ${connectionInstance.connection.host} `
     );
   } catch (error) {
-    console.log("MONGODB connection error", error);
+    console.log("MONGODB connection failed", error);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+const app = express();
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed in index.js file ", err);
+  });
 
-/*
+/* other way to connect database and main connection is given inside 
+db folder in index.js file
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 import express from "express";
